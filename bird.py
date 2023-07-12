582c713f04feab99571a2146771c1f9c736a11c5
rtimoney <ronan.timoney@gmail.com>
2023-07-12 22:24:04 +0100

    First upload 12th July 2023 - Ronan

---
+++ bird.py
@@ -0,0 +1,22 @@
+# CHAPTER 10 - PROGRAMMING OBJECTS
+# PAGE 128
+# COPYING INSTANCES
+
+class bird :
+    "A class to define bird properties."
+
+    count = 0
+
+    def _init_(self , chat) :
+        self.sound = chat
+        bird.count += 1
+
+
+    def talk(self) :
+        return self.sound
+
+# incomplete - to be continued
+
+
+
+
