7fa4cb503f1409402459785c8780f68db7c6dac4
Ronan Timoney <103318517+rtimoney@users.noreply.github.com>
2023-07-15 20:17:37 +0100

    Create Rectangle.py

---
+++ Rectangle.py
@@ -0,0 +1,12 @@
+# CHAPTER 10 - PROGRAMMING OBJECTS
+# PAGE 133
+# DERIVING CLASSES - Shape example 
+
+from Polygon import *
+
+# declare a derived class with a method to return manipulated class variable values
+class Rectangle(Polygon) :
+    "A class to define Rectangle properties."
+
+    def area(self) :
+       return(self.width * self.height)/2
